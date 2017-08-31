4cf5b547c97a9c284aa1ec8c73134d5ab3532fea
Rahul Patil <rahulpatil2387@gmail.com>
2017-08-31 12:17:20 +0530

    Add search in API with query string for Questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,11 @@ class Question < ActiveRecord::Base
   has_many :answers
   belongs_to :user
 
+  def self.search(search)
+    if search
+      where("title LIKE ?", "%#{search}%")
+    else
+      all
+    end
+  end
 end
